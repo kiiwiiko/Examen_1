d5f63e95daad578db7e40e8d89474568f4281155
Arie Quimi <trr.2002j@gmail.com>
2023-05-02 12:48:05 -0500

    Realiza la operacion

--- examen.c
+++ examen.c
@@ -4,9 +4,9 @@
 int main(int argc, char const *argv[])
 {
     int n, sum;
-    do
+    do /*Este es el ciclo que se repetira hasta que el usuario ponga cero*/
     {
-        for (int i = 0; i <= 0; i++)
+        for (int i = 0; i <= 0; i++) /*En este ciclo se realiza la operacion*/
         {
             printf("Escribir el digito:");
             scanf("%i", &n);
@@ -15,8 +15,6 @@ int main(int argc, char const *argv[])
     
     } while (n>0);
     printf("El total de la suma de los numeros es %i", sum);
-       
-    
     getch();
     return 0;
 }
