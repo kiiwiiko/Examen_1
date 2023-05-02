9edcde30b13f2028e81db2a81e5c5cd76f31cdda
Arie Quimi <trr.2002j@gmail.com>
2023-05-02 12:47:39 -0500

    El programa funciona y realiza la operacion

--- examen.c
+++ examen.c
@@ -3,6 +3,20 @@
 
 int main(int argc, char const *argv[])
 {
+    int n, sum;
+    do
+    {
+        for (int i = 0; i <= 0; i++)
+        {
+            printf("Escribir el digito:");
+            scanf("%i", &n);
+            sum = sum + n;
+        }
+    
+    } while (n>0);
+    printf("El total de la suma de los numeros es %i", sum);
+       
+    
     getch();
     return 0;
 }
